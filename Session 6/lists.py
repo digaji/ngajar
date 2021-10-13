6b5abd58f184ce825683f4afb9c979713159565c
Jason J. W <71363607+digaji@users.noreply.github.com>
2021-10-13 11:38:56 +0700

    Uploaded session 6 site + python file

---
+++ Session 6/lists.py
@@ -0,0 +1,43 @@
+# Lists
+
+my_list = [1, 2, 3, "apple", "apple", 1]
+# Ordered + indexed + mutable + allow duplicates
+
+# Length of a list
+# print(len(my_list))
+
+# print(my_list[3])
+
+# Methods
+# my_list.append(5)
+
+# my_list.insert(2, 10)
+
+# ^ Adding things to a list
+
+# x = my_list.pop()
+# print(x)
+
+# my_list.remove(1)
+
+# ^ Removing things from a list
+
+# my_list.sort()
+
+# my_list.reverse()
+
+# ^ Adjusting the order of a list
+
+# print(my_list.count("apple"))
+
+# print(my_list.index("apple"))
+
+# your_list = my_list.copy()
+# your_list.append(5)
+# print(your_list)
+
+# another_list = [3, 2, 1]
+
+# my_list.extend(another_list)
+
+print(my_list)
