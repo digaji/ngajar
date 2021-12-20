7f6741a402cc4e2ed5477630024060e350eebdf2
Jason J. W <71363607+digaji@users.noreply.github.com>
2021-12-20 18:08:25 +0700

    Updated pygame and uploaded session 21 files

--- Session-20/game_functions.py
+++ Session-20/game_functions.py
@@ -2,6 +2,23 @@ import sys
 
 import pygame
 
+
+def check_keydown_events(event, ship):
+    """Keypresses
+    """
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+def check_keyup_events(event, ship):
+    """Key releases
+    """
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
 def check_events(ship):
     """Respond to keypresses and mouse events
     """
@@ -9,16 +26,10 @@ def check_events(ship):
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            check_keydown_events(event, ship)
 
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
 
 def update_screen(ai_settings, screen, ship):
     """Update images on the screen, flip to new screen
